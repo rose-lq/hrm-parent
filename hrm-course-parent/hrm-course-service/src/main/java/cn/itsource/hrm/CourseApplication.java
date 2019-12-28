ff949ecb936f7fdf08ca2a6e2662f1eeea971f50
lq <237191293@qq.com>
2019-12-28 08:55:54 +0800

    提交course

--- hrm-course-parent/hrm-course-service/src/main/java/cn/itsource/hrm/CourseApplication.java
+++ hrm-course-parent/hrm-course-service/src/main/java/cn/itsource/hrm/CourseApplication.java
@@ -3,11 +3,13 @@ package cn.itsource.hrm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan("cn.itsource.hrm.mapper")
 @EnableSwagger2
+@EnableFeignClients("cn.itsource.hrm.client")
 public class CourseApplication {
     public static void main(String[] args) {
         SpringApplication.run(CourseApplication.class,args);
--- hrm-course-parent/hrm-course-service/src/main/java/cn/itsource/hrm/service/impl/CourseTypeServiceImpl.java
+++ hrm-course-parent/hrm-course-service/src/main/java/cn/itsource/hrm/service/impl/CourseTypeServiceImpl.java
@@ -1,11 +1,16 @@
 package cn.itsource.hrm.service.impl;
 
+import cn.itsource.hrm.client.RedisClient;
 import cn.itsource.hrm.domain.CourseType;
 import cn.itsource.hrm.mapper.CourseTypeMapper;
 import cn.itsource.hrm.service.ICourseTypeService;
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,8 +26,41 @@ import java.util.Map;
  */
 @Service
 public class CourseTypeServiceImpl extends ServiceImpl<CourseTypeMapper, CourseType> implements ICourseTypeService {
+
+    @Autowired
+    private RedisClient redisClient;
+    private final String COURSE_TYPE = "hrm:course_type:treeData";
+
     @Override
-    public List<CourseType> localTree(){
+    public List<CourseType> localTree() {
+        //从redis中查询数据
+        String courseTypesStr = redisClient.get(COURSE_TYPE);
+        List<CourseType> list = null;
+        //判断
+        if(StringUtils.isNotEmpty(courseTypesStr)){
+            //如果存在
+            //json字符串转java集合
+            list = JSONObject.parseArray(courseTypesStr, CourseType.class);
+        }else{
+            //如果不存在，则查询数据库
+            list = localTreeData();
+            //list集合转json字符串
+            String jsonStr = JSONObject.toJSONString(list);
+            //保存到redis中
+            redisClient.set(COURSE_TYPE, jsonStr);
+        }
+        //返回数据
+        return list;
+    }
+
+
+
+    /**
+     * map+for循环
+     * @return
+     */
+
+    public List<CourseType> localTreeData(){
         //存放父级
         List<CourseType> firstTypes = new ArrayList<>();
         //查询所有类型
@@ -43,8 +81,42 @@ public class CourseTypeServiceImpl extends ServiceImpl<CourseTypeMapper, CourseT
                 }
             }
         }
-
         return firstTypes;
     }
+
+    /**
+     * 增删改同步操作
+     */
+    private void synOperate() {
+        List<CourseType> list = localTreeData();
+        //list集合转json字符串
+        String jsonStr = JSONObject.toJSONString(list);
+        //保存到redis中
+        redisClient.set(COURSE_TYPE, jsonStr);
+    }
+
+
+    @Override
+    public boolean save(CourseType entity) {
+        super.save(entity);
+        synOperate();
+        return true;
+    }
+
+
+    @Override
+    public boolean removeById(Serializable id) {
+        super.removeById(id);
+        synOperate();
+        return true;
+    }
+
+    @Override
+    public boolean updateById(CourseType entity) {
+        super.updateById(entity);
+        synOperate();
+        return true;
+    }
+
 
 }
